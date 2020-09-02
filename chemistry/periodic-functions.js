8911a3c90a9d8444f448a01c9b808ed91c45258a
Beardger <joshuabreuer@protonmail.com>
2020-09-02 22:41:54 +0200

    Started out; H, He added; added display function

---
+++ chemistry/periodic-functions.js
@@ -0,0 +1,9 @@
+function HydrogenText() {
+  var paragraph = document.getElementById("HydrogenText");
+  paragraph.classList.toggle("Explanations");
+}
+
+function HeliumText() {
+  var paragraph = document.getElementById("HeliumText");
+  paragraph.classList.toggle("Explanations");
+}
