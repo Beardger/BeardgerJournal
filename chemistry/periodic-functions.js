33ae27f134eda258e413b8aa2865eb25c1d5ef65
Beardger <joshuabreuer@protonmail.com>
2020-09-12 20:07:59 +0200

    .

--- chemistry/periodic-functions.js
+++ chemistry/periodic-functions.js
@@ -7,3 +7,43 @@ function HeliumText() {
   var paragraph = document.getElementById("HeliumText");
   paragraph.classList.toggle("Explanations");
 }
+
+function LithiumText() {
+  var paragraph = document.getElementById("LithiumText");
+  paragraph.classList.toggle("Explanations");
+}
+
+function BerylliumText() {
+  var paragraph = document.getElementById("BerylliumText");
+  paragraph.classList.toggle("Explanations");
+}
+
+function BoronText() {
+  var paragraph = document.getElementById("BoronText");
+  paragraph.classList.toggle("Explanations");
+}
+
+function CarbonText() {
+  var paragraph = document.getElementById("CarbonText");
+  paragraph.classList.toggle("Explanations");
+}
+
+function NitrogenText() {
+  var paragraph = document.getElementById("NitrogenText");
+  paragraph.classList.toggle("Explanations");
+}
+
+function OxygenText() {
+  var paragraph = document.getElementById("OxygenText");
+  paragraph.classList.toggle("Explanations");
+}
+
+function FluorideText() {
+  var paragraph = document.getElementById("FluorideText");
+  paragraph.classList.toggle("Explanations");
+}
+
+function NeonText() {
+  var paragraph = document.getElementById("NeonText");
+  paragraph.classList.toggle("Explanations");
+}
